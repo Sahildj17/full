560ebb62bab49e7ba3aba957deff7badaabf9740
Sahildj17 <110554998+Sahildj17@users.noreply.github.com>
2023-04-21 14:14:24 +0530

    Add files via upload

---
+++ exp_1.sh
@@ -0,0 +1,10 @@
+curl --location --request POST 'https://ap-south-1.aws.data.mongodb-api.com/app/data-awblt/endpoint/data/v1/action/findOne' \
+--header 'Content-Type: application/json' \
+--header 'Access-Control-Request-Headers: *' \
+--header 'api-key: gRsC1NvM6Dc9GuqWUdOAJuSdK9Y6StQGNeyJRrD6zTvseFtymhkdr8IyHBjSvMQj' \
+--data-raw '{
+    "collection":"Employee",
+    "database":"Company",
+    "dataSource":"Cluster0",
+    "projection": {"_id": 1,"name":1}
+}'
